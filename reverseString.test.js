dbb9b6f84fc9db5453e374eeefac05239ec96f91
kdncode <kdncode@gmail.com>
2018-05-26 23:18:11 -0500

    reverseString

---
+++ reverseString.test.js
@@ -0,0 +1,9 @@
+const reverseString = require('./reverseString')
+
+test('reverseString func exists', () => {
+    expect(reverseString).toBeDefined();
+})
+
+test('String reverses', () => {
+    expect(reverseString('hello')).toEqual('olleh')
+})
