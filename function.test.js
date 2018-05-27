7050025262ca2eed822062f8c9e0e314f4ee7726
kdncode <kdncode@gmail.com>
2018-05-26 20:59:01 -0500

    null

--- function.test.js
+++ function.test.js
@@ -3,3 +3,7 @@ const functions = require('./functions');
 test('Add 2 + 1 to equal 3', () => {
     expect(functions.add(2, 1)).toBe(3);
 });
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
