4d0fbf8a429f3ce3faa676f772628f88b05ee648
kdncode <kdncode@gmail.com>
2018-05-26 22:12:29 -0500

    Async Await Promise

--- function.test.js
+++ function.test.js
@@ -38,3 +38,21 @@ test('Should be under 1000', () => {
 test('There is no A in hello', () => {
     expect('hello').not.toMatch(/A/);
 })
+
+// Working with Async Data - Axios
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//     expect.assertions(1);
+//     return functions.fetchUser()
+//         .then(data => {
+//             expect(data.name).toEqual('Leanne Graham')
+//         })
+// })
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham')
+})
